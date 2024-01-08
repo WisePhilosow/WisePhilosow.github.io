5d688572f218071bb363f943beb9693fec4a1c8c
WisePhilosow <112717089+WisePhilosow@users.noreply.github.com>
2024-01-08 17:47:27 +0300

    Update screen.js

--- screen.js
+++ screen.js
@@ -1,12 +1,17 @@
 let screenW = screenH = windowW = windowH = false;
- 
+
+var element = document.getElementById("myElement");
+
 var calcScreen = function() {
   screenW = screen.width;
   screenH = screen.height;
   windowW = Math.max(document.documentElement.clientWidth, window.innerWidth);
   windowH = Math.max(document.documentElement.clientHeight, window.innerHeight);
+
+ element = document.documentElement.clientWidth;
 };
  
 calcScreen();
 window.addEventListener("resize", calcScreen());
 window.addEventListener("orientationchange", calcScreen());
+
