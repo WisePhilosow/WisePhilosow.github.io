6a0dcb097084b7f5dcf1f12f34443fee44893b4e
WisePhilosow <112717089+WisePhilosow@users.noreply.github.com>
2024-01-10 19:37:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+window.addEventListener('load',function(){
+    const canvas = this.document.getElementById('canvas1');
+    const ctx =canvas.getContext('2d');
+    canvas.width=500;
+    canvas.height=500;
+
+
+    class Game {
+        constructor(width,height){
+            this.width;
+            this.height;
+        }
+
+        update(){
+
+        }
+
+        draw(){
+
+            
+        }
+    }
+
+
+});
